c443c9a6c1e47962cc779f1c164b2c788a83413b
Vitaly <109906595+brainlessless@users.noreply.github.com>
2023-03-20 17:00:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+//    public static boolean isNegativ(int a) {
+//        if (a >= 0) {
+//            return false;
+//        } else {
+//            return true;
+//        }
+//
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(isNegativ(-4));
+//    }
+
+//    public static boolean isPozitive(int a) {
+//        if (a >= 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(isPozitive(-4));
+//    }
+
+//    public static boolean isZero(int a) {
+//        if (a == 0) {
+//            return true;
+//        }
+//        else {
+//            return false;
+//        }
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(isZero(0));
+//    }
+
+//    public static boolean isEven(int a) {
+//        if (a%2 == 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(isEven(-3));
+//    }
+
+//    public static boolean isNotEven(int a) {
+//        if (a%2 == 0) {
+//            return false;
+//        } else {
+//            return true;
+//        }
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(isNotEven(-4));
+//    }
+
+//    public static int quadro(int a) {
+//        return (a*a);
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(quadro(3));
+//    }
+
+//    public static int triple(int a) {
+//        return (a*a)*a;
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(triple(2));
+//    }
+
+//    public static int degree(int a, int b) {
+//        int result = 1;
+//        for (int i = 1; i <= b; i++) {
+//            result = result * a;
+//        }
+//        return result;
+//
+//
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(degree(3, 5));
+//    }
+
+//    public static int sum(int a, int b) {
+//        return a + b;
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(sum(3, 5));
+//    }
+
+
+
+}
